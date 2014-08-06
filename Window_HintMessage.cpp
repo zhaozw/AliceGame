8e35ec50de2128b117edb8b68237d47a11f57150
Takanashi1443 <silly.formula@gmail.com>
2014-08-06 21:22:02 +0900

    Making class: Window_HintMessage

--- Window_HintMessage.cpp
+++ Window_HintMessage.cpp
@@ -1,6 +1,8 @@
 // Window_HintMessage.cpp
 
 #include "Window_HintMessage.h"
+#include <iostream>
+#include <fstream>
 
 Window_HintMessage::Window_HintMessage() : Window_Message(){
 	// ウィンドウの情報をクリアする。
@@ -12,5 +14,23 @@ void Window_HintMessage::Clear(){
 	_tcscpy_s(fileName, MAX_PATH, _T(""));
 	// ファイル位置リセット
 	filePos = 0;
+}
 
+bool Window_HintMessage::SetFileName(LPTSTR name){
+	_tcscpy_s(fileName, MAX_PATH-1, name);
+	filePos = 0;
+	lineCount = 0;
+
+	// ファイルを開く
+	// (開けるかどうかの確認だけを行う)
+	std::basic_ifstream<TCHAR> fin;
+	fin.open(fileName, std::ios::in);
+	if(!fin){
+		fin.close();
+		return false;
+	}
+
+	// ファイルを閉じる
+	fin.close();
+	return true;
 }
